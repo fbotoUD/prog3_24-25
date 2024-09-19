7dfc8ee363941b226b94f240d3f6c0ff48c210a8
fernando.boto <fernando.boto@INGLT03607.deusto.es>
2024-09-19 14:51:53 +0200

    Ejemplos ventanas

---
+++ Clase/src/es/deusto/prog3/cap03/ejemplos/VentanaSencilla1.java
@@ -0,0 +1,52 @@
+package es.deusto.prog3.cap03.ejemplos;
+
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+public class VentanaSencilla1 {
+
+	/**
+	 *   
+	 * JPanel tiene su propio layout manager (administrador de diseño) que por defecto es un FlowLayout, 
+	 * lo que significa que los componentes se organizan en línea y se ajustan según el tamaño de su contenido y del panel.
+	 */
+
+    public static void main(String[] args) {
+    	JFrame ventana = new JFrame();
+    	// Configuración de la ventana
+    	ventana.setTitle("Envío de Correo Electrónico");
+    	ventana.setSize(400, 200);
+    	ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    	// Crear el panel
+        JPanel panel = new JPanel();
+        ventana.add(panel); // Añadir el panel a la ventana
+        
+
+        // Crear las etiquetas
+        JLabel lMail = new JLabel("Email:");
+        JLabel lMensaje = new JLabel("Escribe aquí tu mensaje:");
+
+        // Crear el campo de texto
+        JTextField tfMail = new JTextField(20);
+
+        // Crear el área de texto
+        JTextArea areaTexto = new JTextArea(5, 15);
+        JScrollPane scrollPane = new JScrollPane(areaTexto); // Añadir scroll
+
+        // Crear los botones
+        JButton boton1 = new JButton("Enviar");
+        JButton boton2 = new JButton("Salir");
+
+        // Añadir los componentes al panel
+        panel.add(lMail);
+        panel.add(tfMail);
+        panel.add(lMensaje);
+        panel.add(scrollPane);
+        panel.add(boton1);
+        panel.add(boton2);
+        
+        ventana.setVisible(true);
+    }
+}
---
+++ Clase/src/es/deusto/prog3/cap03/ejemplos/VentanaSencilla2.java
@@ -0,0 +1,61 @@
+package es.deusto.prog3.cap03.ejemplos;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.*;
+
+public class VentanaSencilla2 {
+
+	/**
+	 *   
+	 * Usando BorderLayout y GridLayout para las tres partes
+	 */
+
+    public static void main(String[] args) {
+    	JFrame ventana = new JFrame();
+    	// Configuración de la ventana
+    	ventana.setTitle("Envío de Correo Electrónico");
+    	ventana.setSize(400, 200);
+    	ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    	// Crear el panel
+        JPanel panel = new JPanel();
+        ventana.add(panel); // Añadir el panel a la ventana
+        panel.setLayout(new BorderLayout());
+        
+
+        // Crear etiqueta mail
+        JLabel lMail = new JLabel("Email:");
+        // Crear el campo de texto
+        JTextField tfMail = new JTextField(20);
+        JPanel panelMail = new JPanel();
+        panelMail.setLayout(new GridLayout(2, 1));
+        panelMail.add(lMail);
+        panelMail.add(tfMail);
+        
+        JLabel lMensaje = new JLabel("Escribe aquí tu mensaje:");
+        // Crear el área de texto
+        JTextArea areaTexto = new JTextArea(5, 15);       
+        JScrollPane scrollPane = new JScrollPane(areaTexto); // Añadir scroll
+        JPanel panelTexto = new JPanel();
+        panelTexto.setLayout(new GridLayout(2, 1));
+        panelTexto.add(lMensaje);
+        panelTexto.add(scrollPane);
+
+        // Crear los botones
+        JButton boton1 = new JButton("Enviar");
+        JButton boton2 = new JButton("Salir");
+        JPanel panelBotones = new JPanel();
+        panelBotones.setLayout(new GridLayout(1, 2));
+        panelBotones.add(boton1);
+        panelBotones.add(boton2);        
+
+        // Añadir los componentes al panel
+        panel.add(panelMail,BorderLayout.NORTH);
+        panel.add(panelTexto,BorderLayout.CENTER);
+        panel.add(panelBotones,BorderLayout.SOUTH);
+        
+        ventana.setVisible(true);
+    }
+}
