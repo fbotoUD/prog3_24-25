32f27eda2812b8ad8b65f799c87eb21b05cc0ed1
Fernando Boto <fernando.boto@deusto.es>
2024-10-14 08:54:54 +0200

    lambdas

--- Clase/src/es/deusto/prog3/cap01/ejemplos/EjemploLambda1.java
+++ Clase/src/es/deusto/prog3/cap01/ejemplos/EjemploLambda1.java
@@ -2,6 +2,8 @@ package es.deusto.prog3.cap01.ejemplos;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.*;
 
@@ -18,11 +20,24 @@ public class EjemploLambda1 {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					System.out.println( e.getWhen() );
+					getNumbers();
 				}
 			}
 //			(e) -> { System.out.println( e.getWhen() );}
 		);
 		ventana.setVisible( true );
+	}
+	private static void getNumbers() {
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+//		for(int i = 0;i<numbers.size();i++) {
+//			System.out.println(numbers.get(i));
+//		}
+		for(int n:numbers) {
+			System.out.println(n);
+		}
+        // Expresión lambda para imprimir cada número
+//        numbers.forEach(number -> System.out.println(number));
 	}
 	public static void main(String[] args) {
 		// ...
